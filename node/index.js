79709875897ef410352ff4023f6eb15e76f63237
Cleber Simm <clebersimm@gmail.com>
2022-05-07 11:46:05 -0300

    Fullcycle desafio 2 docker
    Adicionado proxy reverso com nginx.
    Colocado o entrypoint para o docker-compose,

--- node/index.js
+++ node/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')
 const mysql = require('mysql');
 const app = express();
 const port = 3000;
@@ -9,15 +9,22 @@ const configDatabase = {
     password: 'root',
     database: 'nodedb'
 };
-const connection = mysql.createConnection(configDatabase);
-const sql = `INSERT INTO people(name) values('Cleber')`;
-connection.query(sql);
 
 app.get('/', (req, res) => {
-    const query = 'select name from people';
-    connection.query(query, function(err, result){
-        console.log(result);
-        res.send('<h1>Full Cycle Rocks!</h1>');
+    const connection = mysql.createConnection(configDatabase);
+    connection.query(`INSERT INTO people(name) values('Cleber')`);
+    connection.query('select name from people', function(err, result){
+        if(err) throw err;
+        let output  = '<h1>Full Cycle Rocks!</h1>';
+        output = output.concat('<p>');
+        output = output.concat('<ul>');
+        result.forEach(tmp => {
+            output = output.concat(`<li>${tmp.name}</li>`);
+        });
+        output = output.concat('</ul>');
+        output = output.concat('</p>');
+        
+        res.send(output);
     });
     
 });
